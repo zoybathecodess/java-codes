f80b39426778ccca6430ec560c5c65344050391f
zoyba <zoybaparihan006@gmail.com>
2025-10-14 15:19:44 +0530

    update

---
+++ module3/constarg.java
@@ -0,0 +1,20 @@
+class Student {
+    String name;
+    int age;
+    String course;
+    Student(){
+        System.out.println("no argument constructor");
+    }
+
+    public Student(String name, int age, String course) {
+        this.name = "zoyba";
+        this.age = 19;
+        this.course = "aiml";
+    }
+
+    
+}
+public class constarg {
+
+    
+}
